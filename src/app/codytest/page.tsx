c43116e8c9f2a59bfb5e6bbe22a06b423ef569b6
jeongol <jungcode127@gmail.com>
2024-10-11 16:53:26 +0900

    test: push 3차 테스트

--- src/app/codytest/page.tsx
+++ src/app/codytest/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>pagessssssssssssss=dsdsdsdsd</div>;
+  return <div>pagessssssssssssss=dsdsdsdsdsssssssssssssssdasdasdasdadsasd</div>;
 };
 
 export default page;
