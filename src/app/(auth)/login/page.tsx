defa45ce06305fdd3cb43b141622273da8676379
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-10-11 21:47:55 +0900

    chore: pull전 commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <input />
+    </div>
+  );
+};
 
-export default page
+export default page;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const page = () => {
-  return <div>pagesdsdsadasdss</div>;
+  return (
+    <>
+      <div>회원가입</div>
+      <div>
+        이메일 : <input />
+        비밀번호 : <input />
+        <button>회원가입</button>
+      </div>
+    </>
+  );
 };
 
 export default page;
