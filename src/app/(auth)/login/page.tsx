68e17debcc4adbfd6ca5c9debd8854464bae8980
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2024-10-14 17:30:54 +0900

    add: header, 로그인/회원가입 추가(zustand-상태관리로 로그인정보 저장 완료)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,75 @@
-import React from "react";
+"use client";
 
-const page = () => {
+import supabase from "@/utils/supabase";
+import { useUserStore } from "@/zustand/store";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password
+    });
+
+    if (data.user && data.user.email) {
+      useUserStore.getState().loginUser({
+        email: data.user.email,
+        accessToken: data.session.access_token,
+        userId: data.user.id,
+        isAuthenticated: true
+      });
+      console.log("로그인이 완료되었습니다");
+      router.push("/");
+    } else {
+      console.error("Email이 존재하지 않습니다.");
+    }
+  };
+
   return (
     <div>
-      <input />
+      <h1>로그인</h1>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>
+            이메일:
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="text-black"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            비밀번호:
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="text-black"
+            />
+          </label>
+        </div>
+        <button type="submit">로그인</button>
+      </form>
+      <div>
+        <div>아직 회원이 아니신가요?</div>
+        <Link href={"/signup"}>회원가입 하러가기</Link>
+      </div>
     </div>
   );
 };
 
-export default page;
+export default LoginPage;
